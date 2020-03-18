f0e8919ec7fe6b3c73bdab8bb0955e23b5a0d479
Daniel Gomes Jeronimo Leite <daniel.leite@SERPB.local>
2020-03-18 13:37:26 -0300

    bug corrigido, instantiation funcinando

--- src/main/java/com/danielgomes/demo/Instantiation.java
+++ src/main/java/com/danielgomes/demo/Instantiation.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+
 import java.util.Arrays;
 
 @Configuration
@@ -15,14 +16,18 @@ public class Instantiation implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+
+
         userRepository.deleteAll();
 
         User maria = new User(null, "Daniel Gomes", "daniel@gmail.com");
         User jao = new User(null, "maria", "maria@maria.com");
         User teste = new User(null, "lala", "marlia@maria.com");
 
-        // Corrigir
-        userRepository.save(Arrays.asList(maria, jao, teste));
+
+        userRepository.saveAll(Arrays.asList(maria, jao,teste));
+
+
 
     }
 }
